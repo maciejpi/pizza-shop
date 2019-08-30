409810ea18c2350f38c290347f18bd860a3720e7
Maciej Piosik <m.piosik@gmail.com>
2019-08-30 11:48:45 +0200

    Add redux and store folder; amend HMR for redux;

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import initStore from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = initStore();
+
 const render = Component => {
   return ReactDOM.render(
-    <BrowserRouter>
-      <Component />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <Component />
+      </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
   );
 };
