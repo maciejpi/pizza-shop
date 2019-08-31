a3a9b8421804f8ab29d2c0ebe2c19085fa8567f7
Maciej Piosik <m.piosik@gmail.com>
2019-08-31 13:34:20 +0200

    Calculate and display total price of the order.

--- src/components/orderSummary.js
+++ src/components/orderSummary.js
@@ -4,21 +4,25 @@ import { Link } from 'react-router-dom';
 
 const OrderSummary = ({ pathname }) => {
   const pizzas = useSelector(state => state.selectedPizzas);
+  const totalPrice = pizzas.reduce((total, pizza) => total + pizza.price, 0);
 
   return (
     <>
       <h2>order summary</h2>
       {pizzas.length ? (
-        <div>
-          {pizzas.map(({ price, dough, ingredients }, i) => (
-            <div key={i}>
-              <div>name: pizza {i + 1}</div>
-              <div>price: {price}</div>
-              <div>size: {dough}</div>
-              <div>{ingredients.join(', ')}</div>
-            </div>
-          ))}
-        </div>
+        <>
+          <div>
+            {pizzas.map(({ price, dough, ingredients }, i) => (
+              <div key={i}>
+                <div>name: pizza {i + 1}</div>
+                <div>price: {price}</div>
+                <div>size: {dough}</div>
+                <div>{ingredients.join(', ')}</div>
+              </div>
+            ))}
+          </div>
+          <div>Total: {totalPrice} </div>
+        </>
       ) : (
         <p>Your basket is empty.</p>
       )}
